bff3957f254372c0c404262399c67ba37511c18d
Ratovonirina <eric.ratovonirina@gmail.com>
2023-07-24 21:02:02 +0200

    Link Login and Sign

--- frontend/app/src/main/java/com/example/wm/Login.java
+++ frontend/app/src/main/java/com/example/wm/Login.java
@@ -1,8 +1,18 @@
 package com.example.wm;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +20,22 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        LabelBottom();
+    }
+
+    private void LabelBottom(){
+        TextView label = findViewById(R.id.loglbl);
+        SpannableString ss = new SpannableString(label.getText().toString());
+        ClickableSpan actionClicked = new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View view) {
+                //Toast.makeText(Login.this, "To Sign", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(Login.this, Sign.class);
+                startActivity(intent);
+            }
+        };
+        ss.setSpan(actionClicked, 7, ss.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        label.setText(ss);
+        label.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
--- frontend/app/src/main/java/com/example/wm/Sign.java
+++ frontend/app/src/main/java/com/example/wm/Sign.java
@@ -1,14 +1,39 @@
 package com.example.wm;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Sign extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign);
+        LabelBottom();
+    }
+
+    private void LabelBottom(){
+        TextView label = findViewById(R.id.loglbl);
+        SpannableString ss = new SpannableString(label.getText().toString());
+        ClickableSpan actionClicked = new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View view) {
+                //Toast.makeText(Sign.this, "To login", Toast.LENGTH_LONG);
+                Intent intent = new Intent(Sign.this, Login.class);
+                startActivity(intent);
+            }
+        };
+        ss.setSpan(actionClicked, 0, ss.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        label.setText(ss);
+        label.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
