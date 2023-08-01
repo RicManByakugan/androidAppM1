69a0904e7a92c42fa337da91cb81012abf0af900
Mangidy <45750939+Mangidy@users.noreply.github.com>
2023-08-01 14:14:15 +0300

    User connection done

---
+++ frontend/app/src/main/java/com/example/wm/YourResponseModel.java
@@ -0,0 +1,15 @@
+package com.example.wm;
+
+import com.google.gson.annotations.SerializedName;
+
+public class YourResponseModel {
+
+    @SerializedName("message")
+    String message;
+
+
+
+    // Constructor, getters, and setters
+    // ...
+}
+
--- frontend/app/src/main/java/com/example/wm/connexion/ApiService.java
+++ frontend/app/src/main/java/com/example/wm/connexion/ApiService.java
@@ -15,13 +15,64 @@ import retrofit2.http.Path;
 
 public interface ApiService {
 
+<<<<<<< Updated upstream:frontend/app/src/main/java/com/example/wm/connexion/ApiService.java
+=======
+    // Add the base URL endpoint
+    // Base Endpoint
+    @GET("/")
+    Call<YourResponseModel> getBaseEndpoint();
+
+    // User Endpoints
+    @GET("/user/")
+    Call<YourResponseModel> getUser();
+
+    @GET("/user/notification")
+    Call<YourResponseModel> getUserNotification();
+
+    @GET("/user/preference")
+    Call<YourResponseModel> getUserPreference();
+
+>>>>>>> Stashed changes:frontend/app/src/main/java/com/example/wm/ApiService.java
     @FormUrlEncoded
-    @POST("/user/login") // Make sure the endpoint is correct based on your server API
-    Call<User> loginUser(@Field("logName") String logName, @Field("password") String password);
+    @POST("/user/preference/add")
+    Call<YourResponseModel> addUserPreference(@Field("userId") String userId, @Field("preference") String preference);
+
+    @FormUrlEncoded
+    @POST("/user/notification/add")
+    Call<YourResponseModel> addUserNotification(@Field("message") String message, @Field("user") String user);
+
+    @FormUrlEncoded
+    @POST("/user/subscribe")
+    Call<YourResponseModel> addUserSubscription(@Field("name") String name, @Field("firstname") String firstname,
+                                                @Field("logname") String logname, @Field("password") String password);
+
+    @FormUrlEncoded
+    @POST("/user/login")
+    Call<YourResponseModel> loginUser(@Field("logName") String logName, @Field("password") String password);
+
+    @POST("/user/logout")
+    Call<YourResponseModel> logoutUser();
+
+    // Post Endpoints
+    @GET("/post/:idPost")
+    Call<YourResponseModel> getPost(@Field("idPost") String idPost);
 
     @GET("/post/")
+<<<<<<< Updated upstream:frontend/app/src/main/java/com/example/wm/connexion/ApiService.java
     Call<Post> getAllPost();
 
     @GET("/post/{id}")
     Call<Post> getOnePost(@Path("id") String id);
+=======
+    Call<List<Post>> getAllPosts();
+
+    @FormUrlEncoded
+    @POST("/post/search")
+    Call<YourResponseModel> searchPost(@Field("cleSearch") String cleSearch);
+
+    @FormUrlEncoded
+    @POST("/post/")
+    Call<YourResponseModel> addPost(@Field("param1") String param1, @Field("param2") String param2);
+
+>>>>>>> Stashed changes:frontend/app/src/main/java/com/example/wm/ApiService.java
 }
