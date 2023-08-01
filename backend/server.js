b9018a903cefa0a4429499fa92ffafec5ce95c24
Ratovonirina <eric.ratovonirina@gmail.com>
2023-08-01 12:44:16 +0200

    Port backend

--- backend/server.js
+++ backend/server.js
@@ -2,4 +2,4 @@ const app = require("./app")
 const routeUser = require("./route/routeUser")
 const routePost = require("./route/routePost")
 
-app.start(5000, routeUser, routePost)
+app.start(3000, routeUser, routePost)
