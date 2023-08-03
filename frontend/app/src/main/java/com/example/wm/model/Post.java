7df0f8ee9afd52b5393b1c0a6f835f59ec1a8b99
Ratovonirina <eric.ratovonirina@gmail.com>
2023-08-03 14:52:33 +0200

    Fixed Post Model front

--- frontend/app/src/main/java/com/example/wm/model/Post.java
+++ frontend/app/src/main/java/com/example/wm/model/Post.java
@@ -4,6 +4,8 @@ public class Post {
 
     private String _id = null;
     private String image_url = null;
+
+    private String video_url = null;
     private String title = null;
     private String datePost = null;
     private String Lieu = null;
@@ -11,9 +13,18 @@ public class Post {
     public Post(String _id, String image_url, String title, String datePost, String lieu) {
         this._id = _id;
         this.image_url = image_url;
+        this.video_url = image_url;
         this.title = title;
         this.datePost = datePost;
         Lieu = lieu;
+    }
+
+    public String getVideo_url() {
+        return video_url;
+    }
+
+    public void setVideo_url(String video_url) {
+        this.video_url = video_url;
     }
 
     public String get_id() {
