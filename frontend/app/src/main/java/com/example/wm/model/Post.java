9c2a1a63573a44e394d475c99fd9ab18dabf37aa
Ratovonirina <eric.ratovonirina@gmail.com>
2023-08-05 21:21:34 +0200

    Rectification

--- frontend/app/src/main/java/com/example/wm/model/Post.java
+++ frontend/app/src/main/java/com/example/wm/model/Post.java
@@ -12,14 +12,14 @@ public class Post {
 
     private String visite = null;
 
-    public Post(String _id, String image_url, String title, String datePost, String lieu, String visite) {
+    public Post(String _id, String image_url, String title, String datePost, String lieu) {
         this._id = _id;
         this.image_url = image_url;
         this.video_url = image_url;
         this.title = title;
         this.datePost = datePost;
         this.Lieu = lieu;
-        this.visite = visite;
+       
     }
 
     public String getVisite() {
